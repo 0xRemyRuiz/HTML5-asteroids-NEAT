77e9f67eb48ff136bc9fb8fd2a849f8e663077b7
0xRemyRuiz <remyruiz@protonmail.com>
2025-04-16 23:28:28 +0200

    finished from Set to hash map migration + added base theoretical logic on nodes and connections

--- genetics/neat/network.js
+++ genetics/neat/network.js
@@ -3,26 +3,42 @@ export class Node {
   // id is the unique identifier of the node, it won't change
   id = 0
   // layer 0 is input, layer -1 is output and all the layers in between are hidden nodes layers
-  layer = 0
+  layer = null
   activation_name = null
   activation_func = null
   // innov the innovation value based on how recent the node is and/or the activation mutation
   innov = 0
+  // used to store inputs
+  inputs = []
 
-  constructor(id, layer, activation_name, activation_func) {
+  constructor(id, innov, activation_name, activation_func) {
     this.id = id
-    this.layer = layer
+    this.innov = innov
     this.activation_name = activation_name
     this.activation_func = activation_func
   }
 
-  activate(inputs) {
+  set_layer(layer) {
+    this.layer = layer
+  }
+
+  accumulate(input) {
+    // used to accumulate inputs until neuron gets activated
+    this.inputs.append(input)
+  }
+
+  activate() {
     // TODO: maybe change this to be a parametric agreggation function
     let sum = 0
-    for (const i in inputs) {
-      sum += inputs[i]
+    for (const i in this.inputs) {
+      sum += this.inputs[i]
     }
     return this.activation_func(sum)
+  }
+
+  copy() {
+    // pass over a copy of this node
+    return new Node(this.id, this.innov, this.activation_name, this.activation_func)
   }
 
   get_id() {
@@ -52,7 +68,8 @@ export class Connection {
 export class Network {
   specie = null
   nodes = []
-  connections = []  
+  connections = []
+  layers = []
 
   constructor(specie, nodes, connections) {
     this.specie = specie
@@ -61,11 +78,11 @@ export class Network {
   }
 
   get_random_connection() {
-
+    return this.connections[Math.random() * this.connections.length - 1]
   }
 
   get_random_node() {
-
+    return this.nodes[Math.random() * this.nodes.length - 1]
   }
 
   add_node(connection) {
@@ -73,7 +90,7 @@ export class Network {
   }
 
   add_connection(from, to) {
-    
+
   }
 }
 
